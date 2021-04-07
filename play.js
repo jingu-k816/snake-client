54c69d31484cecc9e3073540e0c102835293d79d
Jingu Kang <jinguk0816@gmail.com>
2021-04-07 13:56:31 -0400

    added client part of the snake game that was hosted by lighthouse labs

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+  
+  conn.setEncoding('utf-8');
+
+  conn.on("data", (data) => {
+    console.log(data);
+  })
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
