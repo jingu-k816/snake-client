7acfed19e8948f282f62a95bc9fe719ec09e698b
Jingu Kang <jinguk0816@gmail.com>
2021-04-07 14:07:32 -0400

    moveed connect function to client server and exported the connect function using property shorthand object method on client.js:

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+  
+  conn.setEncoding('utf-8');
+
+  conn.on("data", (data) => {
+    console.log(data);
+  })
+
+  return conn;
+}
+
+module.exports = {
+  connect
+};
