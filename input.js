60948e5c208e010586723051b39d0fea3282ff59
Jingu Kang <jinguk0816@gmail.com>
2021-04-07 20:09:46 -0400

    added keyboard input that will track the movement inside the server

--- input.js
+++ input.js
@@ -1,19 +1,31 @@
-const setupInput = (data) => {
+let connection;
+
+const handleUserInput = function(stdin) {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    } else if (key === 'w') {
+      connection.write(`Move: up`);
+    } else if (key === 'a') {
+      connection.write(`Move: left`);
+    } else if (key === 's') {
+      connection.write(`Move: down`);
+    } else if (key === 'd') {
+      connection.write(`Move: right`);
+    }
+
+  });
+}
+
+const setupInput = function(conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf-8');
   stdin.resume();
-  
-  const handleUserInput = (key) => {
-    stdin.on('data', (key) => {
-      if(key === '\u0003'){
-        process.exit();
-      }
-    })
-  }
+  handleUserInput(stdin);
 
-  handleUserInput();
-  
   return stdin;
 }
 
